acbf74aa985ccfea3df4cb3a702ed2ed4f9b4587
Yuriy Gudimov <fash1k@gmail.com>
2025-02-27 12:58:10 +0200

    Initial commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Yuriy Gudimov on 27.02.25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
