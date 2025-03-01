e6b7930de89edb22b3435c01a1723366bc106ada
Yuriy Gudimov <fash1k@gmail.com>
2025-03-01 11:16:07 +0100

    Finish header of EpisodeDetailsView

--- StarWars/ViewModels/EpisodeDetailsViewModel.swift
+++ StarWars/ViewModels/EpisodeDetailsViewModel.swift
@@ -9,9 +9,13 @@ import Foundation
 
 extension EpisodeDetailsView {
     final class EpisodeDetailsViewModel: ObservableObject {
-        private let episode: Episode
+        let episode: Episode
         @Published var error: AppError?
         @Published var characters: [Character]?
+        var releaseYear: String {
+            let splittedDate = episode.releaseDate.split(separator: "-")
+            return String(splittedDate[0])
+        }
         
         init(episode: Episode) {
             self.episode = episode
--- StarWars/Views/EpisodeDetailsView/EpisodeDetailsView.swift
+++ StarWars/Views/EpisodeDetailsView/EpisodeDetailsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct EpisodeDetailsView: View {
     @StateObject private var viewModel: EpisodeDetailsViewModel
+    @State private var screenSize: CGSize?
     
     init(episode: Episode) {
         self._viewModel = StateObject(wrappedValue: EpisodeDetailsView.EpisodeDetailsViewModel(episode: episode))
@@ -17,12 +18,78 @@ struct EpisodeDetailsView: View {
     var body: some View {
         ZStack {
             BackgroundView()
+            
+            ScrollView {
+                VStack(alignment: .center, spacing: 10) {
+                    // HEADER VIEW
+                    headerView
+                    
+                    Divider()
+                        .background(Color.accent)
+                    
+                    // CHARACTERS
+                    // DESCRIPTION
+                    
+                    Spacer()
+                }
+                
+            }
+            .overlay(
+                GeometryReader { geometry in
+                    Color.clear
+                        .onAppear {
+                            self.screenSize = geometry.size
+                        }
+                }
+            )
         }
+    }
+    
+    var headerView: some View {
+        HStack {
+            Spacer()
+            
+            VStack {
+                Spacer()
+                
+                // TITLE
+                Text(viewModel.episode.title)
+                    .font(.title)
+                
+                    .lineLimit(2, reservesSpace: true)
+                    .multilineTextAlignment(.center)
+                
+                // RELEASE DATE
+                Text(viewModel.releaseYear)
+                    .font(.headline)
+                    .foregroundStyle(.accent.opacity(0.7))
+                
+                Spacer()
+                
+                // DIRECTOR
+                Text("Directed by:\n\(viewModel.episode.director)")
+                    .font(.headline)
+                    .foregroundStyle(.white.opacity(0.7))
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+            }
+            .foregroundStyle(.accent)
+            
+            Spacer()
+            
+            // DISPLAY IMAGE
+            viewModel.episode.displayImage
+                .resizable()
+                .scaledToFit()
+                .frame(width: (screenSize?.width ?? 500) * 0.35)
+        }
+        .padding(.horizontal, 10)
     }
 }
 
 #Preview {
     let characters = [Character(birthYear: "1966", eyeColor: "Brown", films: [""], gender: "", hairColor: "", height: "", homeworld: "", mass: "", name: "Yuriy Gudimov", skinColor: "", created: Date(), edited: Date(), species: [""], starships: [""], url: "", vehicles: [""])]
-    let episode = Episode(title: "The episode title", releaseDate: "1987-05-25", director: "Yuriy Gudimov", episodeId: 1, openingCrawl: "Long long time ago in a galaxy far, far away...", url: "", characters: characters)
+    let episode = Episode(title: "The Empire Strikes Back", releaseDate: "1987-05-25", director: "Yuriy Gudimov", episodeId: 1, openingCrawl: "Long long time ago in a galaxy far, far away...", url: "", characters: characters)
     EpisodeDetailsView(episode: episode)
 }
