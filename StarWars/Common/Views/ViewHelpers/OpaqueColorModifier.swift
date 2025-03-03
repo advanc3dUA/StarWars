0fa5bac694a84c321c370aac4526d170f7c9b0ff
Yuriy Gudimov <fash1k@gmail.com>
2025-03-03 09:24:35 +0100

    Rename CustomSecondaryColorModifier to OpaqueColorModifier and create func in view extension for easier usage

--- StarWars/Common/Views/ViewHelpers/OpaqueColorModifier.swift
+++ StarWars/Common/Views/ViewHelpers/OpaqueColorModifier.swift
@@ -1,5 +1,5 @@
 //
-//  CustomSecondaryColorModifier.swift
+//  OpaqueColorModifier.swift
 //  StarWars
 //
 //  Created by Yuriy Gudimov on 01.03.25.
@@ -7,9 +7,16 @@
 
 import SwiftUI
 
-struct CustomSecondaryColorModifier: ViewModifier {
+struct OpaqueColorModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .foregroundStyle(.white.opacity(0.7))
+    }
+}
+
+extension View {
+    public func opaqueColor() -> some View {
+        self
+            .modifier(OpaqueColorModifier())
     }
 }
